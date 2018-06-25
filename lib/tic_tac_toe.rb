7718a9d2f1542721b7a9994914ee007e5a6ebff2
Kristen Papke <klpapke215@gmail.com>
2018-06-25 03:21:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,9 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+  
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -10,18 +12,23 @@ class TicTacToe
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+
   def move(board, index, current_player)
     board[index] = current_player
   end
+
   def position_taken?(board, location)
     board[location] != " " && board[location] != ""
   end
+
   def valid_move?(board, index)
     index.between?(0,8) && !position_taken?(board, index)
   end
+
   def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
@@ -33,6 +40,7 @@ class TicTacToe
       turn(board)
     end
   end
+
   def turn_count(board)
    counter = 0
    board.each do |space|

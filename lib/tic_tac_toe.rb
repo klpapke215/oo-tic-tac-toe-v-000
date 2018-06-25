183c859eb2140a9fcedf4c3fd3093cc5dea59c3f
Kristen Papke <klpapke215@gmail.com>
2018-06-25 03:18:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,10 +11,10 @@ class TicTacToe
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
   def input_to_index(user_input)
-  user_input.to_i - 1
+    user_input.to_i - 1
   end
   def move(board, index, current_player)
-  board[index] = current_player
+    board[index] = current_player
   end
-  
+
 end

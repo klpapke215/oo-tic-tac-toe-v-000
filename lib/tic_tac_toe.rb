aa52eef35157e05e02030775a08c577a94675a61
Kristen Papke <klpapke215@gmail.com>
2018-06-25 03:40:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,11 +22,11 @@ class TicTacToe
   end
 
   def position_taken?(location)
-    board[location] != " " && board[location] != ""
+    @board[location] != " " && @board[location] != ""
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn

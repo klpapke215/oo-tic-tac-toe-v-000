053f1bf0db672e0100473ec440ed17cebbc006cb
Kristen Papke <klpapke215@gmail.com>
2018-06-25 03:22:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,4 +59,56 @@ class TicTacToe
     end
   end
 
+  def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      windex_1 = win_combination[0]
+      windex_2 = win_combination[1]
+      windex_3 = win_combination[2]
+
+      position_1 = board[windex_1]
+      position_2 = board[windex_2]
+      position_3 = board[windex_3]
+
+      if board.all? { |index| index == " "}
+        false
+      elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      else
+        false
+      end
+    end
+    false
+  end
+
+  def full?(board)
+    board.all? { |space| space == "X" || space == "O" }
+  end
+
+  def draw?(board)
+    full?(board) && !won?(board)
+  end
+
+  def over?(board)
+    won?(board) && full?(board) || draw?(board) || won?(board) && !full?(board)
+  end
+
+  def winner(board)
+    if winning_combo = won?(board)
+  board[winning_combo[0]]
+    end
+  end
+
+  def play(board)
+    until over?(board)
+      turn(board)
+    end
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
+  end
+
 end
